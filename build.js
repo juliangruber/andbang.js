7208fa9a40f619dfa215a2197636a21bf9cfe1e7
Henrik Joreteg <hjoreteg@gmail.com>
2012-08-28 02:16:18 -0700

    adding full paths for building and minified script

--- build.js
+++ build.js
@@ -1,17 +1,22 @@
 // Creates the andbang.js source from the provided API
 // specification.
 
-var TEMPLATE = "andbang.template.js",
-    OUTPUT = "andbang.js";
+/*global __dirname*/
+var fileName = 'andbang.js',
+    minFileName = 'andbang.min.js',
+    templatePath = __dirname + "/andbang.template.js",
+    outputPath = __dirname + '/' + fileName,
+    minifiedPath = __dirname + '/' + minFileName;
 
 var fs = require('fs'),
     mustache = require('mustache'),
     andbangSpec = require('andbang-spec'),
     yetify = require('yetify'),
+    uglify = require('uglify-js'),
     colors = require('colors');
     
 var methods = andbangSpec.getMethodsByApiType('js'),
-    template = fs.readFileSync(TEMPLATE, 'utf-8'),
+    template = fs.readFileSync(templatePath, 'utf-8'),
     api = {"methods": []};
 
 methods.forEach(function (method) {
@@ -25,8 +30,22 @@ methods.forEach(function (method) {
     });
 });
 
-fs.writeFileSync(OUTPUT, mustache.render(template, api), 'utf-8');
+var code = mustache.render(template, api);
 
 console.log('\n' + yetify.andBangLogo() + ':');
-console.log(OUTPUT.bold + ' file built.'.grey + '\n');
+
+fs.writeFileSync(outputPath, code, 'utf-8');
+console.log(fileName.bold + ' file built.'.grey);
+
+var ast = uglify.parser.parse(code),
+    pro = uglify.uglify,
+    minified;
+
+ast = pro.ast_mangle(ast); // get a new AST with mangled names
+ast = pro.ast_squeeze(ast); // get an AST with compression optimizations
+minified = pro.gen_code(ast); // build out the code
+
+fs.writeFileSync(minifiedPath, minified, 'utf-8');
+
+console.log(minFileName.bold + ' file built.'.grey + '\n');
 process.exit(0);    
